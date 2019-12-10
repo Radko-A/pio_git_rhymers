c63a2ccc58e398c1dda45d8025d5d7a547ba8537
Radko-A <51504206+Radko-A@users.noreply.github.com>
2019-12-10 14:48:36 +0100

    Revert "adnotacje i stale"

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,9 +2,10 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-	int value;
-	Node prev, next;
-	Node(int i) {
+	public int value;
+	public Node prev, next;
+
+	public Node(int i) {
 		value = i;
 	}
 	
