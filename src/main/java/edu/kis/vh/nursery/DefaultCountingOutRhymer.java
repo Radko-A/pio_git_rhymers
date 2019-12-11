015cb8b577b1254a21dee3d0e345dbc65792ad5d
Damian Jacewicz <damian.jacewicz@tomtom.com>
2019-12-11 22:11:01 +0100

    initial -> array_empty in functions count peekaboo

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,6 +4,8 @@ public class DefaultCountingOutRhymer {
 
 	public static final int ARRAY_SIZE = 12;
 	public static final int LAST_ELEMENT = 11;
+	public static final int ARRAY_EMPTY = -1;
+
 	private final int[] NUMBERS = new int[ARRAY_SIZE];
 	private final int INITIAL = -1;
 
@@ -24,13 +26,13 @@ public class DefaultCountingOutRhymer {
 		
 	protected int peekaboo() {
 		if (callCheck())
-			return INITIAL;
+			return ARRAY_EMPTY;
 		return NUMBERS[total];
 	}
 			
 	public int countOut() {
 		if (callCheck())
-			return INITIAL;
+			return ARRAY_EMPTY;
 		return NUMBERS[total--];
 	}
 
