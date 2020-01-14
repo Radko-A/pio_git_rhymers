4763d0da981db8c9f4a83835d0ab411f53ad7113
Anastasiia <radko22nastya@gmail.com>
2020-01-14 05:42:07 +0100

    Dodana dokumentacja dla HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,26 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Class is used in RhymersFactory
+ */
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+	/**
+	 * field for amount of rejected
+	 */
 	int totalRejected = 0;
+
+	/**
+	 *
+	 * @return number of rejected
+	 */
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
 			totalRejected++;
